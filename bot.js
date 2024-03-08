e282895781ae72bbd9b64a66b34395dd3f1d797d
Niecoo <nicoswaves.mn@gmail.com>
2024-03-08 15:51:13 +0300

    double cross

--- bot.js
+++ bot.js
@@ -1,4 +1,55 @@
-// bot.js
+const Binance = require("node-binance-api");
+const { buy, sell } = require("./helpers");
+const logger = require("./logger");
+
+require("dotenv").config();
+
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
+const symbol = "BTCUSDT";
+
+const buyThreshold = 5;
+const sellThreshold = 5.5;
+
+const binance = new Binance().options({
+  APIKEY: apiKey,
+  APISECRET: apiSecret,
+  family: 4,
+});
+
+async function checkPriceAndTrade() {
+  try {
+    const prevDay = await binance.prevDay(symbol);
+    const lowPrice = parseFloat(prevDay.lowPrice);
+    const currentPrice = parseFloat(prevDay.lastPrice);
+
+    if (currentPrice <= lowPrice * buyThreshold) {
+      await buy(binance, symbol, currentPrice);
+    }
+
+    if (currentPrice >= currentPrice * sellThreshold) {
+      await sell(binance, symbol, currentPrice);
+    }
+  } catch (error) {
+    console.log(apiKey);
+    console.log(apiSecret);
+    logger.error("Error fetching previous day data:", error);
+  }
+}
+
+// Run the trading logic continuously
+(async () => {
+  while (true) {
+    await checkPriceAndTrade();
+    await delay(5 * 60 * 1000); // Delay for 5 minutes
+  }
+})();
+
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+/* // bot.js
 const Binance = require("node-binance-api");
 const { buy, sell } = require("./helpers");
 const logger = require("./logger");
@@ -46,3 +97,4 @@ function checkPriceAndTrade() {
 async function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+ */
